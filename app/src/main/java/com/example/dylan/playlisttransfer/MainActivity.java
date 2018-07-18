88e05af9fc3a8835cf783153234fccbe48b55361
Dylan Nguyen <dh.dvnguyen@gmail.com>
2018-07-17 20:37:41 -0700

    added Spotify login

--- app/src/main/java/com/example/dylan/playlisttransfer/MainActivity.java
+++ app/src/main/java/com/example/dylan/playlisttransfer/MainActivity.java
@@ -1,14 +1,35 @@
 package com.example.dylan.playlisttransfer;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.spotify.sdk.android.authentication.AuthenticationClient;
+import com.spotify.sdk.android.authentication.AuthenticationRequest;
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
+import com.spotify.sdk.android.player.Config;
+import com.spotify.sdk.android.player.ConnectionStateCallback;
+import com.spotify.sdk.android.player.Error;
+import com.spotify.sdk.android.player.Player;
+import com.spotify.sdk.android.player.PlayerEvent;
+import com.spotify.sdk.android.player.Spotify;
+import com.spotify.sdk.android.player.SpotifyPlayer;
 
 import org.w3c.dom.Text;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements SpotifyPlayer.NotificationCallback, ConnectionStateCallback{
+    private static final String CLIENT_ID = "5047539c966a4a5ca3e43e6b47937493";
+    private static final String REDIRECT_URI = "playlisttransfer://callback";
+    private static final int REQUEST_CODE = 458;
+    private String authToken = "";
+    private Player mPlayer;
+    private String[] scopes = new String[]{"user-read-private", "playlist-read-private", "playlist-read-collaborative"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,5 +49,97 @@ public class MainActivity extends Activity {
             stringBuilder.append(someMessage);
         }
         messageWindow.setText(stringBuilder.toString());
+
+        // Spotify Authentication
+        SpotifyLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AuthenticationRequest.Builder builder = new AuthenticationRequest.Builder(CLIENT_ID, AuthenticationResponse.Type.TOKEN, REDIRECT_URI);
+                builder.setScopes(scopes);
+                AuthenticationRequest request = builder.build();
+
+                AuthenticationClient.openLoginActivity(MainActivity.this, REQUEST_CODE, request);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        if (requestCode == REQUEST_CODE) {
+            AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, intent);
+            if (response.getType() == AuthenticationResponse.Type.TOKEN) {
+                authToken = response.getAccessToken();
+                Config playerConfig = new Config(this, authToken, CLIENT_ID);
+                Spotify.getPlayer(playerConfig, this, new SpotifyPlayer.InitializationObserver() {
+                    @Override
+                    public void onInitialized(SpotifyPlayer spotifyPlayer) {
+                        mPlayer = spotifyPlayer;
+                        mPlayer.addConnectionStateCallback(MainActivity.this);
+                        mPlayer.addNotificationCallback(MainActivity.this);
+                    }
+
+                    @Override
+                    public void onError(Throwable throwable) {
+                        Log.e("MainActivity", "Could not initialize player: " + throwable.getMessage());
+                    }
+                });
+            }
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        Spotify.destroyPlayer(this);
+        super.onDestroy();
+    }
+
+    @Override
+    public void onPlaybackEvent(PlayerEvent playerEvent) {
+        Log.d("MainActivity", "Playback event received: " + playerEvent.name());
+        switch (playerEvent) {
+            // Handle event type as necessary
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onPlaybackError(Error error) {
+        Log.d("MainActivity", "Playback error received: " + error.name());
+        switch (error) {
+            // Handle error type as necessary
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onLoggedIn() {
+        Log.d("MainActivity", "User logged in");
+        TextView loginText = findViewById(R.id.loginText);
+        loginText.setText("Logged in");
+        Toast.makeText(getApplicationContext(), "Logged in", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onLoggedOut() {
+        Log.d("MainActivity", "User logged out");
+    }
+
+    @Override
+    public void onLoginFailed(Error var1) {
+        Log.d("MainActivity", "Login failed");
+    }
+
+    @Override
+    public void onTemporaryError() {
+        Log.d("MainActivity", "Temporary error occurred");
+    }
+
+    @Override
+    public void onConnectionMessage(String message) {
+        Log.d("MainActivity", "Received connection message: " + message);
     }
 }
